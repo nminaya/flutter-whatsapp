881b8a95a3966c4e5d5b3ada51eb51133b31d11a
nminaya <nelsonminayaperez@gmail.com>
2018-05-19 20:09:10 -0400

    Adding Tabs

--- lib/whatsapp_home.dart
+++ lib/whatsapp_home.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
 
-class WhatsAppHome extends StatefulWidget{
+class WhatsAppHome extends StatefulWidget {
   @override
   _WhatsAppHomeState createState() => new _WhatsAppHomeState();
 }
 
-class _WhatsAppHomeState extends State<WhatsAppHome>{
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
   @override
-  Widget build(BuildContext context){
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, initialIndex: 1, length: 4);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("WhatsApp"),
         elevation: 0.7,
-
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(text: "CHATS"),
+            Tab(text: "STATUS"),
+            Tab(text: "CALLS")
+          ],
+        ),
       ),
+      body: Container(),
     );
   }
 }
